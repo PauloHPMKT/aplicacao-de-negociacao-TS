f752ac367326ca4eccfda28a8ae5335723956327
Paulo Sergio <pauloserg861@gmail.com>
2022-06-20 11:25:36 -0300

    adicionando controller

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,10 @@
-import { Negociacao } from "./models/negociacao.js"
+import { NegociacaoController } from "./controllers/negociacao.controller.js";
 
-const negociacao = new Negociacao(new Date(), 10, 100, 1000)
-console.log(negociacao.volume)
+const controller = new NegociacaoController()
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e:Event) => {
+    e.preventDefault();
+    controller.adicionar();
+});
---
+++ app/controllers/negociacao.controller.ts
@@ -0,0 +1,18 @@
+export class NegociacaoController {
+    private inputData;
+    private inputQuantidade;
+    private inputValor;
+
+    constructor() {
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade');
+        this.inputValor = document.querySelector('#valor');
+    }
+
+    adicionar() {
+        console.log(this.inputData);
+        console.log(this.inputQuantidade);
+        console.log(this.inputValor);
+
+    }
+}
